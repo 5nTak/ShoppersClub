07620d325e2d3ff5c14510fca1403307a0ba7d06
Tak <oit5538@gmail.com>
2021-11-25 16:35:11 +0900

    refactor: Item 모델 일부 수정 - id 프로퍼티 추가 - descriptions, images 프로퍼티 옵셔널타입으로 변경 - registrationDate 프로퍼티 Double타입으로 변경

--- ShoppersClub/Model/Response/Item.swift
+++ ShoppersClub/Model/Response/Item.swift
@@ -8,18 +8,19 @@
 import Foundation
 
 struct Item: Decodable {
+    let id: Int
     let title: String
-    let descriptions: String
+    let descriptions: String?
     let price: UInt
     let currency: String
     let stock: UInt
     let discountedPrice: UInt?
     let thumbnails: [String]
-    let images: [String]
-    let registrationDate: UInt
+    let images: [String]?
+    let registrationDate: Double
     
     enum CodingKeys: String, CodingKey {
-        case title, descriptions, price, currency, stock, thumbnails, images
+        case id, title, descriptions, price, currency, stock, thumbnails, images
         case discountedPrice = "discounted_price"
         case registrationDate = "registration_date"
     }
