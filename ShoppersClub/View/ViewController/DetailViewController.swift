45b286cfa97005448f8573d377a00988440c5a43
Tak <oit5538@gmail.com>
2022-01-15 14:51:07 +0900

    feat: DetailViewController 생성

---
+++ ShoppersClub/View/ViewController/DetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DetailViewController.swift
+//  ShoppersClub
+//
+//  Created by Tak on 2022/01/15.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
