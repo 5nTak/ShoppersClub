9fa4bab387a298b3cca80b5d9eef15638795ce9b
Tak <oit5538@gmail.com>
2022-01-17 00:19:38 +0900

    feat: DetailViewController Delegate 프로토콜 채택

---
+++ ShoppersClub/View/DetailView/ViewController/Extension/collectionViewDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.swift
+//  ShoppersClub
+//
+//  Created by Tak on 2022/01/17.
+//
+
+import UIKit
+
+extension DetailViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: DetailImageViewCell.cellId, for: indexPath) as? DetailImageViewCell else { return UICollectionViewCell() }
+        guard let images = item?.images else { return UICollectionViewCell() }
+        cell.configureItemImages(with: images[indexPath.row])
+        return cell
+    }
+}
